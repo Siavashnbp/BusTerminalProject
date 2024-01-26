19d590bbc7a5aa0c75297fda53376b58e7101819
Siavash Nabipour <51988240+Siavashnbp@users.noreply.github.com>
2024-01-26 15:05:14 +0330

    EntitiesAndRelations

---
+++ BusTerminalProject/EnitityMap/BusEntityMap.cs
@@ -0,0 +1,16 @@
+﻿using BusTerminalProject.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BusTerminalProject.EnitityMap
+{
+    public class BusEntityMap : IEntityTypeConfiguration<Bus>
+    {
+        public void Configure(EntityTypeBuilder<Bus> builder)
+        {
+            builder.HasKey(_ => _.Id);
+            builder.Property(_ => _.Id).UseIdentityColumn();
+            builder.Property(_ => _.Name).HasMaxLength(50);
+        }
+    }
+}
