a3d7a2b1036b8385e10ea89e606dc2fe5ce76804
Siavash Nabipour <51988240+Siavashnbp@users.noreply.github.com>
2024-01-29 09:24:44 +0330

    Models Refactor

---
+++ BusTerminalProject/Migrations/20240129054819_FieldChanges.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusTerminalProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class FieldChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalIncome",
+                table: "Trips");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalIncome",
+                table: "Trips",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
